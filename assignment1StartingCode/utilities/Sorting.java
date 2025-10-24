05a8e34fa9da308e7559db249b62cb00fb77dca0
foxdog2104 <nyxreidy@gmail.com>
2025-10-24 01:18:54 -0600

    combsort functionality

--- assignment1StartingCode/utilities/Sorting.java
+++ assignment1StartingCode/utilities/Sorting.java
@@ -7,7 +7,6 @@ import shapes.Shape;
 public final class Sorting {
 
     private Sorting() {}
-
     public static void bubbleSort(Shape[] arr, Comparator<Shape> comp) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
@@ -30,5 +29,39 @@ public final class Sorting {
                 }
             }
         }
+    }
+    private static getNextGap(int gap) {
+    	gap = (gap*10)/13;
+    	if (gap < 1) {
+    		return 1;
+    	}
+    	return gap;
+    }
+    public void combSort(Shape[] inArray, Comparator<java.awt.Shape> comp) {
+    	int aLength = inArray.length;
+    	
+    	int gap = aLength;
+    	
+    	boolean swapped = true;
+    	
+	    while (gap != 1 || swapped == true) {
+	    	gap = getNextGap(gap);
+	    	swapped = false;
+	    		
+	    	for (int i=0; i<(aLength-gap); i++) { //comparison
+	    		int result;
+	    		if (comp == null) { //if no comparator sorts by height (default)
+	    			result = inArray[i].compareTo(inArray[i+gap]);
+	    		} else { //sorts by comparators sort method
+	    			result = comp.compare(inArray[i], inArray[i+gap]);
+	    		}
+	    		if (result > 0) {
+	    			int temp = inArray[i];
+	    			inArray[i] = inArray[i+gap];
+	    			inArray[i+gap] = temp;
+	    			swapped = true;
+	    		}
+	    	}
+	    }
     }
 }
